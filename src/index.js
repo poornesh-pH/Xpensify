97dc99792d94b4b0d5e65d785415cc019f18b654
poornesh-pH <poornesh9626@gmail.com>
2021-06-12 21:01:16 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import AppRouter from './Router/AppRouter';
 import configureStore from './Redux/store/store';
 import { addExpense } from './Redux/actions/expenseAct';
-import { setTextField } from './Redux/actions/filtersRed';
+import { setTextField } from './Redux/actions/filtersAct';
 import getVisibleExpense from './Redux/selectors/expensesSel';
 const store = configureStore();
 const expense1 = store.dispatch(
@@ -18,7 +18,9 @@ const expense2 = store.dispatch(
     description: 'Rent'
   })
 );
-const sortByText = store.dispatch(setTextField({text:'ent'}))
-console.log(store.getState());
-// console.log(getVisibleExpense())
+const sortByText = store.dispatch(setTextField({text:"ent"}))
+// console.log(store.getState());
+const state = store.getState();
+const visibleExpeses = getVisibleExpense(state.expenses,state.filters);
+console.log(visibleExpeses);
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
