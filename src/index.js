d4402f5ca0d329f6054c59fea0b2f7e2fe1a1ca6
poornesh-pH <poornesh9626@gmail.com>
2021-06-12 14:56:58 +0530

    1 file modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './Router/AppRouter';
+import configureStore from './Redux/store/store';
 
+const store = configureStore();
+console.log(store.getState())
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
