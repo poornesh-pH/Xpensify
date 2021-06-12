6ab54b0a6a0259005f399607b4a1b261ea3a8704
poornesh-pH <poornesh9626@gmail.com>
2021-06-12 19:46:14 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './Router/AppRouter';
 import configureStore from './Redux/store/store';
-
+import addExpense from './Redux/actions/expense'
 const store = configureStore();
 console.log(store.getState())
+
+store.dispatch(addExpense())
+
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
