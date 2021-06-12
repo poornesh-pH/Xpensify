1dcadda5b8c547dbf0c09dc62b5af6353da64bc6
poornesh-pH <poornesh9626@gmail.com>
2021-06-12 20:05:45 +0530

    1 file modified, 5 added, 5 deleted

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './Router/AppRouter';
 import configureStore from './Redux/store/store';
-import addExpense from './Redux/actions/expense'
+import {addExpense} from './Redux/actions/expenseAct';
+import {setTextField} from './Redux/actions/filtersRed';
+import getVisibleExpense from './Redux/selectors/expensesSel';
 const store = configureStore();
-console.log(store.getState())
 
-store.dispatch(addExpense())
+console.log(getVisibleExpense())
+
+const expense1 = store.dispatch(
+  addExpense({
+    amount: 5000,
+    note: 'this is grocery expense',
+    description: 'Grocery',
+    createdAt: 0
+  })
+);
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
