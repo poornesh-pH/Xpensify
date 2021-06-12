3d03ca1442b2b0d54501675c35bb24655fb275f5
poornesh-pH <poornesh9626@gmail.com>
2021-06-12 12:58:06 +0530

    4 files added, 1 deleted

---
+++ src/Redux/actions/expense.js
@@ -0,0 +1,34 @@
+//ADD_EXPENSE
+export const addExpense = ({
+  amount = 0,
+  note = '',
+  description = '',
+  createdAt = 0
+} = {}) => {
+  return {
+    type: 'ADD_EXPENSE',
+    expense: {
+      id: uuidv4(),
+      amount,
+      note,
+      description,
+      createdAt
+    }
+  };
+};
+//EDIT_EXPENSE
+export const editExpense = (id, updates) => {
+  return {
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+  };
+};
+
+//REMOVE_EXPENSE
+export const removeExpense = ({ id } = {}) => {
+  return {
+    type: 'REMOVE_EXPENSE',
+    id
+  };
+};
---
+++ src/Redux/actions/filters.js
@@ -0,0 +1,34 @@
+//FILTER_BY_TEXT
+export const setTextField = ({ text = '' } = {}) => {
+  return {
+    type: 'FILTER_BY_TEXT',
+    text
+  };
+};
+
+//SORT_BY_DATE
+export const sortByDate = () => {
+  return {
+    type: 'SORT_BY_DATE'
+  };
+};
+//SORT_BY_AMOUNT
+export const sortByAmount = () => {
+  return {
+    type: 'SORT_BY_AMOUNT'
+  };
+};
+//SET_START_DATE
+export const setStartDate = startDate => {
+  return {
+    type: 'SET_START_DATE',
+    startDate
+  };
+};
+//SET_END_DATE
+export const setEndDate = endDate => {
+  return {
+    type: 'SET_END_DATE',
+    endDate
+  };
+};
