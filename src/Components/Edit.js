0a72415a1d342d940991ee9bc9db8623308de9ad
poornesh-pH <poornesh9626@gmail.com>
2021-06-19 13:30:07 +0530

    2 files modified

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import {connect} from 'react-redux'
-import ExpenseForm from './ExpenseForm'
-import {editExpense} from '../Redux/actions/expenseAct'
-function Edit(props){
-  console.log(props.expense,'edit')
-  return(
-    <div>
-    <p>Edit Expense {props.match.params.id}</p>
-    <ExpenseForm expense = {props.expense}
-    onSubmit = {(expense)=>{
-     props.dispatch(editExpense(props.expense.id,expense))
-      props.history.push('/')
-    }}/>
-    </div>
-  )
-}
-const mapStateToProps =(state,props)=>{
-  return{
-    expense: state.expenses.find((expense)=> expense.id === props.match.params.id)
-  }
-}
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../Redux/actions/expenseAct';
+import { removeExpense } from '../Redux/actions/expenseAct';
 
-export default connect(mapStateToProps)(Edit)
+function Edit(props) {
+  return (
+    <div>
+    <h3>Edit Expense</h3>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={expense => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push('/');
+        }}
+      />
+      <button
+        onClick={() => {
+          props.dispatch(removeExpense(props.expense.id));
+           props.history.push('/');
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
+}
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(
+      expense => expense.id === props.match.params.id
+    )
+  };
+};
+
+export default connect(mapStateToProps)(Edit);
--- src/Components/ExpenseListItems.js
+++ src/Components/ExpenseListItems.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {removeExpense} from '../Redux/actions/expenseAct';
 import {Link} from 'react-router-dom';
 
 const ExpenseListItems = ({id,description,amount,createdAt,dispatch}) => {
@@ -8,9 +7,7 @@ const ExpenseListItems = ({id,description,amount,createdAt,dispatch}) => {
     <div>
       <Link to ={`/edit/${id}`}>{description}</Link>
       <p>{amount} - {createdAt}</p>
-      <button onClick = {()=>{
-        dispatch(removeExpense(id))
-      }}>Remove</button>
+     
     </div>
   );
 };
