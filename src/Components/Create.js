b0b8eafce4f5ac04721d0af71b8e178ab4c128f9
poornesh-pH <poornesh9626@gmail.com>
2021-06-19 11:03:37 +0530

    2 files modified

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import ExpenseForm from './ExpenseForm';
-export default function Create() {
+import {addExpense} from '../Redux/actions/expenseAct'
+function Create(props) {
   return (
     <div>
       <h3>Add Expense</h3>
       <ExpenseForm onSubmit={(expense)=>{
-        console.log("Add_expense",expense)
+       props.dispatch(addExpense(expense))
+       props.history.push('/')
       }}/>
     </div>
   );
 }
+export default connect()(Create)
