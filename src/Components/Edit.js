1c0fcdfd470133f8e71504403e2c6acf5c6f613d
poornesh-pH <poornesh9626@gmail.com>
2021-06-19 11:47:12 +0530

    1 file modified

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import {connect} from 'react-redux'
+import ExpenseForm from './ExpenseForm'
 function Edit(props){
-  console.log(props,"edit component")
+  console.log(props,'edit')
   return(
     <div>
-    <p>Edit page {props.match.params.id}</p>
+    <p>Edit Expense {props.match.params.id}</p>
+    <ExpenseForm expense = {props.expense}/>
     </div>
   )
 }
+const mapStateToProps =(state,props)=>{
+  return{
+    expense: state.expenses.find((exp)=> exp.id === props.match.params.id)
+  }
+}
 
-export default Edit
+export default connect(mapStateToProps)(Edit)
